011cc13900ebb324134ffdb45b239501ca2c6fc8
lshep <lori.shepherd@roswellpark.org>
2025-07-03 08:50:38 -0400

    Add initial script

---
+++ scripts.sh
@@ -0,0 +1,193 @@
+ssh biocbuild@nebbiolo2
+
+
+##########################################
+##
+## Data Experiment
+##
+##########################################
+
+cd bbs-3.22-data-experiment/MEAT0/
+find . -size +100M -ls
+
+
+ 21128455 2844676 -rw-rw-r--   1 biocbuild biocbuild 2912940864 Apr  8 08:07 ./ChIPXpressData/inst/extdata/DB_GPL570.bigmemory
+ 21128453 1563756 -rw-rw-r--   1 biocbuild biocbuild 1601278008 Apr  8 08:07 ./ChIPXpressData/inst/extdata/DB_GPL1261.bigmemory
+ 21128409 2741496 -r--r--r--   1 biocbuild biocbuild 2807285353 Apr  8 08:07 ./ChIPXpressData/.git/objects/82/1c5eef718ca7b5ae2a82110f6d0de26f53608f
+ 21128405  665596 -r--r--r--   1 biocbuild biocbuild  681565330 Apr  8 08:02 ./ChIPXpressData/.git/objects/4c/b04743b28df30b2e531747d48ca2ca16ad5fdd
+ 21239582  321728 -rw-rw-r--   1 biocbuild biocbuild  329449155 Apr  8 08:44 ./rRDPData/inst/extdata/fungalits_unite/genus_wordConditionalProbList.txt
+ 21239576  113832 -rw-rw-r--   1 biocbuild biocbuild  116563405 Apr  8 08:44 ./rRDPData/inst/extdata/16srrna/genus_wordConditionalProbList.txt
+ 21239529  108208 -r--r--r--   1 biocbuild biocbuild  110802804 Apr  8 08:44 ./rRDPData/.git/objects/04/925b8813a870464ee0ce5bc0fe2cd2157e5984
+ 21234629  132528 -rw-rw-r--   1 biocbuild biocbuild  135706876 Apr  8 08:26 ./FlowSorted.Blood.450k/data/FlowSorted.Blood.450k.rda
+ 21234582  132568 -r--r--r--   1 biocbuild biocbuild  135747043 Apr  8 08:26 ./FlowSorted.Blood.450k/.git/objects/34/7f45e7d495c49243fa0091764bca7bcad38bbb
+ 21366764  268388 -r--r--r--   1 biocbuild biocbuild  274822846 Apr  8 09:03 ./systemPipeRdata/.git/objects/pack/pack-071ae17e6d21618f6849e9af216ecfd5df64849a.pack
+ 21236523  326152 -rw-rw-r--   1 biocbuild biocbuild  333973300 Apr  8 08:33 ./Affyhgu133aExpr/inst/extdata/data.h5
+ 21236492  324100 -r--r--r--   1 biocbuild biocbuild  331873373 Apr  8 08:33 ./Affyhgu133aExpr/.git/objects/e4/a797312494fb1b98857667a4f7f3af4316c068
+ 21237306  126580 -rw-rw-r--   1 biocbuild biocbuild  129615280 Apr  8 08:38 ./FlowSorted.DLPFC.450k/data/FlowSorted.DLPFC.450k.rda
+ 21237264  126616 -r--r--r--   1 biocbuild biocbuild  129653624 Apr  8 08:38 ./FlowSorted.DLPFC.450k/.git/objects/1b/bfdf786a9075cf825e3bb023c44f05c55bbb54
+ 21120872  187568 -rw-rw-r--   1 biocbuild biocbuild  192062196 Apr  8 07:44 ./hapmapsnp6/data/crlmmResult.rda
+ 21120833  187616 -r--r--r--   1 biocbuild biocbuild  192118202 Apr  8 07:44 ./hapmapsnp6/.git/objects/39/bbdfea3017f001f51a3a575c4864b173a2c14a
+ 21131066  109624 -rw-rw-r--   1 biocbuild biocbuild  112250356 Apr  8 08:14 ./Hiiragi2013/data/a.rda
+ 21130980  109660 -r--r--r--   1 biocbuild biocbuild  112284507 Apr  8 08:14 ./Hiiragi2013/.git/objects/2e/b98aff84186eab7393c6d1dfdb91ebcba71d4a
+ 21130876  431064 -rw-rw-r--   1 biocbuild biocbuild  441404104 Apr  8 08:13 ./HD2013SGI/data/featuresPerWell.rda
+ 21130766  431184 -r--r--r--   1 biocbuild biocbuild  441527339 Apr  8 08:13 ./HD2013SGI/.git/objects/5b/3b3b21f4a0a09d950b727da36c79b6385e9a65
+ 21242348  289412 -rw-rw-r--   1 biocbuild biocbuild  296350200 Apr  8 08:49 ./curatedBreastData/data/curatedBreastDataExprSetList.rda
+ 21242290  289496 -r--r--r--   1 biocbuild biocbuild  296439178 Apr  8 08:49 ./curatedBreastData/.git/objects/1a/0870e5aca5528448731fe5db9b0e04dec7872c
+ 21130571  242356 -rw-rw-r--   1 biocbuild biocbuild  248172110 Apr  8 08:11 ./ConnectivityMap/data/rankMatrix.RData
+ 21130532  242400 -r--r--r--   1 biocbuild biocbuild  248210370 Apr  8 08:11 ./ConnectivityMap/.git/objects/d5/d1c2b30519c569d188881e44538b6172762164
+ 21131542  110536 -rw-rw-r--   1 biocbuild biocbuild  113187947 Apr  8 08:17 ./MMDiffBamSubset/inst/extdata/reads/Resc_2.bam
+ 21131538  389724 -rw-rw-r--   1 biocbuild biocbuild  399072930 Apr  8 08:17 ./MMDiffBamSubset/inst/extdata/reads/Input.bam
+ 21131479  389844 -r--r--r--   1 biocbuild biocbuild  399193721 Apr  8 08:16 ./MMDiffBamSubset/.git/objects/da/2ae9243abb93890e2947fc492c19a7c62f7a61
+ 21131487  110572 -r--r--r--   1 biocbuild biocbuild  113222253 Apr  8 08:16 ./MMDiffBamSubset/.git/objects/00/ec061779ce51f29714e47b23be5bb310760742
+ 21239344  388264 -rw-rw-r--   1 biocbuild biocbuild  397575597 Apr  8 08:42 ./ListerEtAlBSseq/inst/extdata/mc_h1_tabix.txt.gz
+ 21239346  274776 -rw-rw-r--   1 biocbuild biocbuild  281364132 Apr  8 08:42 ./ListerEtAlBSseq/inst/extdata/mc_i90_tabix.txt.gz
+ 21239299  388380 -r--r--r--   1 biocbuild biocbuild  397696105 Apr  8 08:41 ./ListerEtAlBSseq/.git/objects/f0/4cb7f64c5295cc3ffc6dd65288715bfaac5ca7
+ 21239303  274860 -r--r--r--   1 biocbuild biocbuild  281449404 Apr  8 08:42 ./ListerEtAlBSseq/.git/objects/d4/3e23fb7d90a2902327a43ca90462d0fd59bf28
+ 21128930  195760 -r--r--r--   1 biocbuild biocbuild  200456683 Apr  8 08:08 ./pRolocdata/.git/objects/pack/pack-34f792bf99753e619ae33b83890b0059ebaaf3eb.pack
+ 21374767  490364 -rw-rw-r--   1 biocbuild biocbuild  502128640 Apr  8 09:21 ./msPurityData/inst/extdata/library_spectra/library_spectra.db
+ 21374720  214040 -r--r--r--   1 biocbuild biocbuild  219169230 Apr  8 09:21 ./msPurityData/.git/objects/c6/ec4ff3ef0ab83ed405b5ed15183b1f982a3af0
+ 23988190  148032 -r--r--r--   1 biocbuild biocbuild  151581822 Apr  8 09:33 ./AssessORFData/.git/objects/pack/pack-327f28e4ed0e884690de7589adcc8557416d6bab.pack
+ 21379010  103300 -r--r--r--   1 biocbuild biocbuild  105775508 Apr  8 09:31 ./topdownrdata/.git/objects/pack/pack-8d2f22935bdbe71a3f8f5373e2ab441ddd4ebeb9.pack
+ 25298289  505644 -r--r--r--   1 biocbuild biocbuild  517773965 Apr  8 09:34 ./macrophage/.git/objects/pack/pack-c914d423976be96aa72c5c71b3cfadca7002f6ed.pack
+ 21368574  246968 -rwxrwxr-x   1 biocbuild biocbuild  252888748 Apr  8 09:08 ./furrowSeg/data/opto.rda
+ 21368573  109848 -rw-rw-r--   1 biocbuild biocbuild  112477492 Apr  8 09:08 ./furrowSeg/data/exampleFurrowMovie.rda
+ 21368463  246980 -r--r--r--   1 biocbuild biocbuild  252902886 Apr  8 09:08 ./furrowSeg/.git/objects/2b/4d66c54ca0890ee7ac90ac541bd7f8951e1465
+ 21368459  109876 -r--r--r--   1 biocbuild biocbuild  112511713 Apr  8 09:07 ./furrowSeg/.git/objects/57/a9f662725e259988116a39c4ff38efb45ce56e
+ 21369444  827808 -rw-rw-r--   1 biocbuild biocbuild  847670348 Apr  8 09:10 ./Single.mTEC.Transcriptomes/data/corMatsNoMarker.RData
+ 21369238  828060 -r--r--r--   1 biocbuild biocbuild  847928084 Apr  8 09:10 ./Single.mTEC.Transcriptomes/.git/objects/88/6750b35e3fab1857db1d6e2c67fc330fd03d8b
+ 21236927  231844 -rw-rw-r--   1 biocbuild biocbuild  237400952 Apr  8 08:36 ./Affyhgu133Plus2Expr/inst/extdata/data.h5
+ 21236895  230104 -r--r--r--   1 biocbuild biocbuild  235618602 Apr  8 08:36 ./Affyhgu133Plus2Expr/.git/objects/3b/179c028a3716d378b516930f68b5b07cddfd3c
+ 21132172  340844 -r--r--r--   1 biocbuild biocbuild  349016698 Apr  8 08:19 ./RNAseqData.HNRNPC.bam.chr14/.git/objects/pack/pack-2ab8127d5029fd83dfad72681a4a5f182fbce75a.pack
+ 21240618  382176 -r--r--r--   1 biocbuild biocbuild  391341092 Apr  8 08:47 ./WES.1KG.WUGSC/.git/objects/pack/pack-defbb55fbeec8eca8c61e73f81b30f20aa36c453.pack
+ 21128345  218116 -rw-rw-r--   1 biocbuild biocbuild  223346688 Apr  8 08:01 ./pd.atdschip.tiling/inst/extdata/pd.atdschip.tiling.sqlite
+ 21128306  110496 -r--r--r--   1 biocbuild biocbuild  113144300 Apr  8 08:01 ./pd.atdschip.tiling/.git/objects/d9/6ddd8eb24139b56797cc4d060ab514081bda18
+ 21364978  106892 -rw-rw-r--   1 biocbuild biocbuild  109455031 Apr  8 08:55 ./SVM2CRMdata/inst/extdata/H3K4me3.sort.txt.bz2
+ 21367527  188188 -r--r--r--   1 biocbuild biocbuild  192704213 Apr  8 09:05 ./mtbls2/.git/objects/pack/pack-910814ecc762b0a1b0e74d7665a8fcf2977f954a.pack
+ 27664454  191716 -r--r--r--   1 biocbuild biocbuild  196313335 Apr  8 09:34 ./oct4/.git/objects/pack/pack-260f2e2bc59d3d5425f5551f6a7a56993787fe19.pack
+ 21372335  227052 -rw-rw-r--   1 biocbuild biocbuild  232496452 Apr  8 09:17 ./FlowSorted.CordBlood.450k/data/FlowSorted.CordBlood.450k.rda
+ 21372294  227120 -r--r--r--   1 biocbuild biocbuild  232566015 Apr  8 09:17 ./FlowSorted.CordBlood.450k/.git/objects/18/e6b9e8b028b03ae788049a834e5a223adb964e
+ 21120688  307596 -rw-rw-r--   1 biocbuild biocbuild  314974121 Apr  8 07:42 ./msdata/inst/proteomics/TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01-20141210.mzML.gz
+ 21120586  307688 -r--r--r--   1 biocbuild biocbuild  315065098 Apr  8 07:41 ./msdata/.git/objects/68/44993e713417379597c32f6009068b016bbe3c
+ 21236610  430996 -rw-rw-r--   1 biocbuild biocbuild  441335730 Apr  8 08:35 ./Affymoe4302Expr/inst/extdata/data.h5
+ 21236578  428236 -r--r--r--   1 biocbuild biocbuild  438508469 Apr  8 08:35 ./Affymoe4302Expr/.git/objects/29/d518037f37c13689de937df38f6ceb0358d486
+ 21233686  150640 -rwxrwxr-x   1 biocbuild biocbuild  154249195 Apr  8 08:24 ./Fletcher2013b/data/rtni1st.RData
+ 21135017  150440 -r--r--r--   1 biocbuild biocbuild  154048665 Apr  8 08:24 ./Fletcher2013b/.git/objects/cb/d21d1a11832105294a6da4bd1f5bea0c378811
+ 21373758  111668 -rwxrwxr-x   1 biocbuild biocbuild  114344330 Apr  8 09:18 ./GeuvadisTranscriptExpr/inst/extdata/genotypes_CEU_chr19.tsv
+ 21366128  113020 -rw-rw-r--   1 biocbuild biocbuild  115725903 Apr  8 08:59 ./RnBeads.hg38/data/CpG.RData
+ 21366086  107852 -r--r--r--   1 biocbuild biocbuild  110438825 Apr  8 08:59 ./RnBeads.hg38/.git/objects/b4/ab4bb2ea52c3fc467113a768caa4de73b8203a
+ 21371741  285412 -r--r--r--   1 biocbuild biocbuild  292254681 Apr  8 09:16 ./tximportData/.git/objects/pack/pack-999d940fa576da42cf62ee824d54b6c287a95b1f.pack
+ 21111891  204840 -rw-rw-r--   1 biocbuild biocbuild  209750812 Apr  8 07:33 ./davidTiling/data/davidTiling.rda
+ 21111715  204896 -r--r--r--   1 biocbuild biocbuild  209805693 Apr  8 07:32 ./davidTiling/.git/objects/d8/24306bade678b178939c97f58979f586b64cd1
+ 21377107  169424 -rw-rw-r--   1 biocbuild biocbuild  173486312 Apr  8 09:28 ./FlowSorted.CordBloodNorway.450k/data/FlowSorted.CordBloodNorway.450k.rda
+ 21377081  169472 -r--r--r--   1 biocbuild biocbuild  173537984 Apr  8 09:28 ./FlowSorted.CordBloodNorway.450k/.git/objects/26/d41d8167f0cf39e5e02eb90d0ceebe9c253bb5
+ 21233902  276488 -r--r--r--   1 biocbuild biocbuild  283123370 Apr  8 08:25 ./chipenrich.data/.git/objects/pack/pack-d08791c58cf278a9fe80984bd6fd10d4cc1f0acb.pack
+ 27676476  268268 -r--r--r--   1 biocbuild biocbuild  274703709 Apr  8 09:39 ./brgedata/.git/objects/pack/pack-c09d9c7eb4117366febd408413e417f9e743c9e2.pack
+ 21132505  218944 -r--r--r--   1 biocbuild biocbuild  224196656 Apr  8 08:19 ./mammaPrintData/.git/objects/pack/pack-babfed3b5bf66076978f27a850f7b976ba154e54.pack
+ 21242398  320260 -r--r--r--   1 biocbuild biocbuild  327940038 Apr  8 08:51 ./rcellminerData/.git/objects/pack/pack-973318bbab6f2581a7683fcbefc0c6b2591b04ca.pack
+ 21375898  569972 -rw-rw-r--   1 biocbuild biocbuild  583646592 Apr  8 09:27 ./ccdata/data/l1000_es.rda
+ 21375900  138080 -rw-rw-r--   1 biocbuild biocbuild  141393824 Apr  8 09:27 ./ccdata/data/net2.rda
+ 21375899  138052 -rw-rw-r--   1 biocbuild biocbuild  141363108 Apr  8 09:27 ./ccdata/data/net1.rda
+ 21375851  138092 -r--r--r--   1 biocbuild biocbuild  141406109 Apr  8 09:26 ./ccdata/.git/objects/9d/4ff07b17919cb1aa61d7eb29a854ba5b33ecd7
+ 21375848  570148 -r--r--r--   1 biocbuild biocbuild  583824058 Apr  8 09:26 ./ccdata/.git/objects/cd/626cdd9c8f360c9641ddc9914a5e21bd97d74b
+ 21375853  138124 -r--r--r--   1 biocbuild biocbuild  141436835 Apr  8 09:27 ./ccdata/.git/objects/e5/fadd24851245aace5a3353552de40032a9416b
+ 21235307  259540 -rw-rw-r--   1 biocbuild biocbuild  265762598 Apr  8 08:29 ./ChAMPdata/data/testDataSet.rda
+ 21235183  257988 -r--r--r--   1 biocbuild biocbuild  264173391 Apr  8 08:29 ./ChAMPdata/.git/objects/36/47f0c68a5ea7d2bdd24ce08cd8fc94985c0058
+
+
+find . -size +100M -ls | cut -d'/' -f2 | sort -u
+
+
+Affyhgu133aExpr
+Affyhgu133Plus2Expr
+Affymoe4302Expr
+AssessORFData
+brgedata
+ccdata
+ChAMPdata
+chipenrich.data
+ChIPXpressData
+ConnectivityMap
+curatedBreastData
+davidTiling
+Fletcher2013b
+FlowSorted.Blood.450k
+FlowSorted.CordBlood.450k
+FlowSorted.CordBloodNorway.450k
+FlowSorted.DLPFC.450k
+furrowSeg
+GeuvadisTranscriptExpr
+hapmapsnp6
+HD2013SGI
+Hiiragi2013
+ListerEtAlBSseq
+macrophage
+mammaPrintData
+MMDiffBamSubset
+msdata
+msPurityData
+mtbls2
+oct4
+pd.atdschip.tiling
+pRolocdata
+rcellminerData
+RNAseqData.HNRNPC.bam.chr14
+RnBeads.hg38
+rRDPData
+Single.mTEC.Transcriptomes
+SVM2CRMdata
+systemPipeRdata
+topdownrdata
+tximportData
+WES.1KG.WUGSC
+
+
+##########################################
+##
+## Workflows
+##
+##########################################
+
+cd bbs-3.22-workflows/meat
+find . -size +100M -ls
+
+ 21925076 110544 -rwxrwxr-x   1 biocbuild biocbuild 113193462 Apr 11 12:46 ./methylationArrayAnalysis/inst/extdata/ageData.RData
+ 21758148 223832 -rw-r--r--   1 biocbuild biocbuild 229198511 Jul  1 13:19 ./methylationArrayAnalysis_1.33.1.tar.gz
+
+find . -size +100M -ls | cut -d'/' -f2 | sort -u
+
+methylationArrayAnalysis
+
+
+
+##########################################
+##
+## Software
+##
+##########################################
+
+cd /bbs-3.22-bioc/meat
+
+find . -size +100M -ls
+
+ 26263259 102876 -rw-rw-r--   1 biocbuild biocbuild 105343816 Jul  2 16:07 ./BASiCStan/src/stanExports_basics_regression_nospikes.o
+ 26263279 104772 -rwxrwxr-x   1 biocbuild biocbuild 107281784 Jul  2 16:07 ./BASiCStan/src/BASiCStan.so
+ 26263143 117888 -rw-rw-r--   1 biocbuild biocbuild 120712960 Jul  2 16:05 ./BASiCStan/src/stanExports_basics_regression.o
+ 26381613 163220 -rwxrwxr-x   1 biocbuild biocbuild 167130872 Jul  2 15:38 ./IgGeneUsage/src/IgGeneUsage.so
+ 26503809 300804 -rw-rw-r--   1 biocbuild biocbuild 308016296 Apr  4 14:43 ./SwathXtend/inst/files/Swath_result_ext.xlsx
+ 26641103 110380 -rwxrwxr-x   1 biocbuild biocbuild 113022920 Jul  2 15:24 ./beachmat/src/beachmat.so
+ 26874057 114776 -rwxrwxr-x   1 biocbuild biocbuild 117524840 Jul  2 15:26 ./mzR/src/mzR.so
+ 26873404 120888 -rw-rw-r--   1 biocbuild biocbuild 123788520 Jul  2 15:15 ./ppcseq/src/stanExports_negBinomial_MPI.o
+ 27032945 116276 -rwxrwxr-x   1 biocbuild biocbuild 119058528 Jul  2 15:30 ./scrapper/src/scrapper.so
+ 26269870 354796 -rw-r--r--   1 biocbuild biocbuild 363304295 Jul  2 19:41 ./SwathXtend_2.31.0.tar.gz
+
+ 
+find . -size +100M -ls | cut -d'/' -f2 | sort -u
+
+BASiCStan
+beachmat
+IgGeneUsage
+mzR
+ppcseq
+scrapper
+SwathXtend
+SwathXtend_2.31.0.tar.gz
