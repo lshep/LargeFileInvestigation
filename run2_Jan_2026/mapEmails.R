41e66358d2eea5db83817e297edf16576cd1b319
lshep <lori.shepherd@roswellpark.org>
2026-01-28 11:48:39 -0500

    Update with information from run1

--- run2_Jan_2026/mapEmails.R
+++ run2_Jan_2026/mapEmails.R
@@ -7,30 +7,36 @@ url_base = "https://pkgmaintainers.bioconductor.org/info/package/"
 ## function to extract maintainer info
 ## Had chatgpt alter original look up
 extract_maintainers <- function(file_name) {
-  pkglist <- read.delim(file_name)[,1]
+  pkglist <- read.delim(file_name, header = FALSE)[,1]
   
   pkg_maintainers <- list()
   
   for (pkg in pkglist) {
     endpoint_url <- paste0(url_base, pkg)
     
+    maintainer_df <- data.frame(
+      package = pkg,
+      name = NA,
+      email = NA,
+      email_status = NA,
+      notes = NA,
+      stringsAsFactors = FALSE
+    )
+    
     try({
       response <- request(endpoint_url) %>% req_perform()
       json_txt <- resp_body_string(response)
       json_data <- fromJSON(json_txt)
       
-      # Initialize dataframe to store maintainers for this package
-      maintainer_df <- NULL
-      
       if (!is.null(json_data$email)) {
-        # Single maintainer at top level
+        # Single maintainer
         maintainer_df <- data.frame(
           package = pkg,
           name = json_data$name,
           email = json_data$email,
           email_status = ifelse(!is.null(json_data$email_status),
                                 json_data$email_status, NA),
-          notes = NA, 
+          notes = NA,
           stringsAsFactors = FALSE
         )
       } else if (length(json_data) > 0 && "email" %in% names(json_data[[1]])) {
@@ -40,20 +46,17 @@ extract_maintainers <- function(file_name) {
           name = sapply(json_data, function(x) x$name),
           email = sapply(json_data, function(x) x$email),
           email_status = sapply(json_data, function(x)
-              ifelse(!is.null(x$email_status), x$email_status, NA)),
-          notes = NA, 
+            ifelse(!is.null(x$email_status), x$email_status, NA)),
+          notes = NA,
           stringsAsFactors = FALSE
         )
       }
-      
-      if (!is.null(maintainer_df)) {
-        pkg_maintainers[[pkg]] <- maintainer_df
-      }
-      
     }, silent = TRUE)
+    
+    pkg_maintainers[[pkg]] <- maintainer_df
   }
   
-  # Combine all into a long dataframe
+  # Combine all into a single dataframe
   bind_rows(pkg_maintainers)
 }
 
